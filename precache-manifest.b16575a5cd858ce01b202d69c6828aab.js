adef67e5536ac13947cf7cbbc92319be8ff6cda3
9999starlight <sestakirena@gmail.com>
2019-10-07 11:43:03 +0200

    Updates

---
+++ precache-manifest.b16575a5cd858ce01b202d69c6828aab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e1b9b2f4970c48da11aff11bac239a",
+    "url": "/movie-list/index.html"
+  },
+  {
+    "revision": "01bbc04a80784cdcac56",
+    "url": "/movie-list/static/css/main.07e373d4.chunk.css"
+  },
+  {
+    "revision": "fdba718708d04b783268",
+    "url": "/movie-list/static/js/2.a902647f.chunk.js"
+  },
+  {
+    "revision": "01bbc04a80784cdcac56",
+    "url": "/movie-list/static/js/main.ad56f7ac.chunk.js"
+  },
+  {
+    "revision": "36f48aa2e01fb851131e",
+    "url": "/movie-list/static/js/runtime-main.93493d0c.js"
+  }
+]);
