c0e3d3548f78829a3118fec1dcf3f55b2dafaab2
9999starlight <sestakirena@gmail.com>
2019-10-07 12:28:58 +0200

    Updates

--- precache-manifest.b3815c1cc91a3cdcb2dd35382857fee7.js
+++ precache-manifest.b3815c1cc91a3cdcb2dd35382857fee7.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c17948a8c22fe639882444e560ddb9d7",
+    "revision": "bf778e1df65ce546e73455451fcc461a",
     "url": "/movie-list/index.html"
   },
   {
-    "revision": "36430e6acdbe1d0cb414",
-    "url": "/movie-list/static/css/main.07e373d4.chunk.css"
+    "revision": "14af32dc7a6a0854a3c8",
+    "url": "/movie-list/static/css/main.4effcf56.chunk.css"
   },
   {
     "revision": "9c5f85f5c018adc20588",
     "url": "/movie-list/static/js/2.643c4387.chunk.js"
   },
   {
-    "revision": "36430e6acdbe1d0cb414",
+    "revision": "14af32dc7a6a0854a3c8",
     "url": "/movie-list/static/js/main.241ce4a4.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-list/precache-manifest.cd7dce884691b7638e269dcdab9fffce.js"
+  "/movie-list/precache-manifest.b3815c1cc91a3cdcb2dd35382857fee7.js"
 );
 
 self.addEventListener('message', (event) => {
